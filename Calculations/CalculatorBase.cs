e8eca2024ff64e664bb8f8122420c32e21bbb03e
grzesieklagocki <grzesieklagocki@gmail.com>
2017-11-07 21:10:30 +0100

    CalculatorBase - dodano ParseFromString i ParseFromString

--- Calculations/CalculatorBase.cs
+++ Calculations/CalculatorBase.cs
@@ -6,25 +6,41 @@ using System.Threading.Tasks;
 
 namespace Calculations
 {
-    internal abstract class CalculatorBase<T>
+    public abstract class CalculatorBase<T>
     {
+        public static Dictionary<string, Func<T>> globalVariables;
+
         protected Dictionary<string, Func<T>> localVariables;
-        protected static Dictionary<string, Func<T>>  globalVariables;
+
+        private readonly ArithmeticFunction[] arithmeticFunctions;
 
 
-        public CalculatorBase()
-        {
-            this.localVariables = new Dictionary<string, Func<T>>();
-        }
+        #region Constructors
 
-        public CalculatorBase(Dictionary<string, Func<T>> localVariables)
+        internal CalculatorBase() : this(new Dictionary<string, Func<T>>()) { }
+
+        internal CalculatorBase(Dictionary<string, Func<T>> localVariables)
         {
             this.localVariables = localVariables;
+
+            arithmeticFunctions = DefineArithmeticFunctions();
         }
 
         static CalculatorBase()
         {
             globalVariables = new Dictionary<string, Func<T>>();
+        }
+
+        #endregion
+
+
+        protected abstract ArithmeticFunction[] DefineArithmeticFunctions();
+
+        protected abstract T ParseFromString(string argument);
+
+        protected virtual string ParseToString(T argument)
+        {
+            return argument.ToString();
         }
 
         #region Register / Unregister Local Variables
@@ -42,6 +58,70 @@ namespace Calculations
             if (!localVariables.Remove(name))
                 throw new Exception($"Nie można wyrejestrować lokalnej zmiennej. \"{name}\" nie zostało wcześniej zarejestrowane");
                 
+        }
+
+        #endregion
+
+        public T Calculate(string formula)
+        {
+            if (formula == null)
+                throw new ArgumentNullException();
+
+
+
+            return default(T);
+        }
+
+        #region Check Formula
+
+        void CheckFormula(string formula)
+        {
+            string[] braces = { "(", ")" };
+
+            var variables = formula
+                .Split(arithmeticFunctions.Select(f => f.Operator).ToArray(), StringSplitOptions.RemoveEmptyEntries)
+                .Concat(braces);
+
+        }
+
+        #endregion
+
+        #region Check Formula Helpers
+
+        private bool CheckVariableNames(string[] names)
+        {
+            return names.All(n => IsValidVariableName(n));
+        }
+
+        private bool IsValidVariableName(string name)
+        {
+            return name.All(c => Char.IsLetterOrDigit(c) || c == '_');
+        }
+
+        private bool IsOperator(string c)
+        {
+            return arithmeticFunctions.Any(f => f.Operator == c);
+        }
+
+        private bool IsCharBracket(char c)
+        {
+            return (c == '(' || c == ')');
+        }
+
+        #endregion
+
+        #region class ArithmeticFunction
+
+        protected class ArithmeticFunction
+        {
+            public string Operator { get; }
+            public Func<T, T, T> Method { get; }
+
+            public ArithmeticFunction(string _operator, Func<T, T, T> method)
+            {
+                Operator = _operator;
+                Method = method;
+            }
         }
 
         #endregion
---
+++ Calculations/CalculatorFloat.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculations
+{
+    public class CalculatorFloat : CalculatorBase<float>
+    {
+        protected override ArithmeticFunction[] DefineArithmeticFunctions()
+        {
+            ArithmeticFunction[] functions =
+            {
+                new ArithmeticFunction("*", (a, b) => a * b),
+                new ArithmeticFunction("/", (a, b) => a / b),
+                new ArithmeticFunction("+", (a, b) => a + b),
+                new ArithmeticFunction("-", (a, b) => a - b),
+            };
+
+            return functions;
+        }
+
+        protected override float ParseFromString(string argument)
+        {
+            return float.Parse(argument);
+        }
+    }
+}
